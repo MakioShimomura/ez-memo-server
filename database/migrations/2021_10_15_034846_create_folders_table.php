a762cd7629447ec02c29393e93274ebbfc388fbb
mshimomura <mshimomura@fourmix.co.jp>
2021-10-18 12:44:56 +0900

    migrage

--- database/migrations/2021_10_15_034846_create_folders_table.php
+++ database/migrations/2021_10_15_034846_create_folders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMemosTable extends Migration
+class CreateFoldersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateMemosTable extends Migration
      */
     public function up()
     {
-        Schema::create('memos', function (Blueprint $table) {
+        Schema::create('folders', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->bigInteger('user_id')->nullable();
-            $table->uuid('folder_id')->nullable();
-            $table->string('title');
-            $table->text('contents');
-            $table->boolean('is_public')->default(false);
-            $table->boolean('is_archive')->default(false);
+            $table->bigInteger('user_id');
+            $table->string('name');
+            $table->uuid('parent_id')->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateMemosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('memos');
+        Schema::dropIfExists('folders');
     }
 }
